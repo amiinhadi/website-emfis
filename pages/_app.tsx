fdbafd297d65f5caed8e4a26ce40c7d3677f5e78
Amiinhadi <amiin.hadi00@gmail.com>
2026-06-16 12:40:21 +0800

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>EMFIS</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
+
